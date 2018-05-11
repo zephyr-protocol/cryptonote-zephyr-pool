54f428bd3333525b01cd8eadb4fabc55f1db309b
zephyr-protocol <jonjonsnow11@gmail.com>
2018-05-11 16:18:09 +0300

    Initial Commit

---
+++ website/config.js
@@ -0,0 +1,17 @@
+var api = "https://pool.ombre.io:8888";
+
+var poolHost = "pool.ombre.io";
+
+var irc = "irc.freenode.net/#ombre";
+
+var email = "support@ombre.io";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://explorer.ombre.io/block/{id}";
+
+var transactionExplorer = "https://explorer.ombre.io/tx/{id}";
+
+var themeCss = "themes/default-theme.css";
